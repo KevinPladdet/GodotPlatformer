818655dff090ef2e49759cb0aeffd12e22bccf0a
KevinPladdet <91886057+KevinPladdet@users.noreply.github.com>
2024-03-22 12:47:32 +0100

    Death Animation Fixed

--- Frog.gd
+++ Frog.gd
@@ -5,6 +5,9 @@ var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
 var player
 var chase = false
 
+func _ready():
+	get_node("AnimatedSprite2D").play("Idle")
+
 func _physics_process(delta):
 	velocity.y += gravity * delta
 	if chase == true:
@@ -35,6 +38,7 @@ func _on_player_detection_body_exited(body):
 
 func _on_player_death_body_entered(body):
 	if body.name == "Player":
+		chase = false
 		get_node("AnimatedSprite2D").play("Death")
 		await get_node("AnimatedSprite2D").animation_finished
 		self.queue_free()
